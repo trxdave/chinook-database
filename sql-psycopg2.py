c8eae8c421896ca0135ae90b883f1a9381fe4572
David Cotter <daveguy28@gmail.com>
2024-05-14 10:25:24 +0000

    cinook setting up

---
+++ sql-psycopg2.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+cursor.execute('SELECT * FROM "Artist"')
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(results)
